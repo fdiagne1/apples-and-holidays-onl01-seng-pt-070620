80da795c03847b476757dffe6bd91ee9a7a8d391
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-19 02:10:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,12 +54,9 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   
-  holiday_hash.each do |season, holidays_hash|
-    if season == :fall
-      holidays_hash << holiday_name << supply_array
+  holiday_hash[season]
 end
-end
-end
+
 
 
 
