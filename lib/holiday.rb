f0b0248539c5592dc03cf29efbde138e7688392b
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-19 02:59:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,12 +85,14 @@ def all_supplies_in_holidays(holiday_hash)
   end
 end
 
-# def all_holidays_with_bbq(holiday_hash)
-#   # return an array of holiday names (as symbols) where supply lists
-#   # include the string "BBQ"
-
-# end
-
+def all_holidays_with_bbq(holiday_hash)
+  # return an array of holiday names (as symbols) where supply lists
+  # include the string "BBQ"
+  
+  holiday_hash.map
+end
+end
+end
 
 
 
