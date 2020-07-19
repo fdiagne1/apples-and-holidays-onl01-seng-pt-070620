17a5eaa6062364b8e829b8a55b9f5b9f1719965d
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-19 02:23:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capatalize!}:" 
     holiday.each do |event, supplies|
-    puts "#{event.to_s.capatalize!}"
+    puts "#{event.to_s.split.capatalize!}"
     end
   end
 end
