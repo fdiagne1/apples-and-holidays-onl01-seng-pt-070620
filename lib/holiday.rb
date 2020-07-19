15390a0b503b788837a801233b9fe85256c3fcc6
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-19 03:09:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,7 +92,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |event, supplies|
       if supplies.include?("BBQ")
-        event
+        event.compact
 end
 end
 end.flatten
